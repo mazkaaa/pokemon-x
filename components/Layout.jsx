986568922ff862badf6f8cf9b9e9cfda2bff6206
Muhammad Azka <azkabdg@gmail.com>
2021-09-11 01:17:04 +0700

    Update

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,10 +7,6 @@ export default function Layout(props) {
       <Head>
         {/* eslint-disable-next-line react/destructuring-assignment */}
         <title>{props.title}</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600&display=swap" rel="stylesheet" />
       </Head>
       {/* eslint-disable-next-line react/destructuring-assignment */}
       {props.children}
